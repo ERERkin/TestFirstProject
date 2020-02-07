3837468b9c665dae65675a258713de4b8488c0bc
ERERkin <erkin1399@gmail.com>
2020-02-08 00:22:03 +0600

    Added test branch

---
+++ src/main/java/com/model/Products.java
@@ -0,0 +1,14 @@
+package com.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class Products {
+    int Id;
+    String code;
+    String name;
+    double price;
+    String url;
+}
