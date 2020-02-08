72e69757a25bf03c865c12430e8825a65c742211
ERERkin <erkin1399@gmail.com>
2020-02-08 10:07:37 +0600

    Added methods in BD addProduct, addDayStatistic, getDayStatistic(return arrayList<ProductShow), getPeriodStatisticForProduct, getDayStatisticCount and getThisDayStatistic

--- src/main/java/com/DB/DB.java
+++ src/main/java/com/DB/DB.java
@@ -1,8 +1,7 @@
 package com.DB;
 
-import com.model.Application;
-import com.model.ProductAmount;
-import com.model.UserStatistic;
+import com.model.*;
+import com.outInformation.ProductShow;
 import lombok.SneakyThrows;
 
 import java.sql.*;
@@ -24,7 +23,7 @@ public class DB {
         }
         return conn;
     }
-    public boolean addApplication(Application application) throws SQLException {
+    public static boolean addApplication(Application application) throws SQLException {
         String SQL = "insert into application (full_name,email,course,is_online)";
         try(Connection connection = connect();
             PreparedStatement statement = connection.prepareStatement(SQL);) {
@@ -39,7 +38,7 @@ public class DB {
         }
         return false;
     }
-    public boolean addUserStatistic(UserStatistic userStatistic)  {
+    public static boolean addUserStatistic(UserStatistic userStatistic)  {
         String SQL = "insert into user_statistic (login, email, storyName, password) values (?,?,?,?)";
         try(Connection connection = connect();
             PreparedStatement statement = connection.prepareStatement(SQL);) {
@@ -54,18 +53,128 @@ public class DB {
         }
         return false;
     }
-    public boolean addProductAmount(ProductAmount productAmount)  {
-        String SQL = "insert into product_amounts (login, email, storyName, password) values (?,?,?,?)";
+    public static boolean addProductAmount(ProductAmount productAmount)  {
+        String SQL = "insert into product_amounts (product_name, day_statistic_id,amountInStock, amountSold) values (?,?,?,?)";
         try(Connection connection = connect();
             PreparedStatement statement = connection.prepareStatement(SQL);) {
-            statement.setString(1,productAmount.getProductCode());
-            statement.setInt(2,productAmount.getAmountInStock());
-            statement.setInt(3,productAmount.getAmountSold());
+            statement.setString(1,productAmount.getProductName());
+            statement.setInt(2,productAmount.getDayStatisticId());
+            statement.setInt(3,productAmount.getAmountInStock());
+            statement.setInt(4,productAmount.getAmountSold());
             statement.executeUpdate();
             return true;
         } catch (SQLException e){
             e.printStackTrace();
         }
         return false;
+    }
+
+    public static boolean addProduct(Product product)  {
+        String SQL = "insert into product (code, name, price, url) values (?,?,?,?)";
+        try(Connection connection = connect();
+            PreparedStatement statement = connection.prepareStatement(SQL);) {
+            statement.setString(1,product.getCode());
+            statement.setString(2,product.getName());
+            statement.setDouble(3,product.getPrice());
+            statement.setString(4,product.getUrl());
+            statement.executeUpdate();
+            return true;
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+        return false;
+    }
+    public static boolean addDayStatistic(DayStatistic dayStatistic)  {
+        String SQL = "insert into dayStatistic (user_login, day_count) values (?,?)";
+        try(Connection connection = connect();
+            PreparedStatement statement = connection.prepareStatement(SQL);) {
+            statement.setString(1,dayStatistic.getUserLogin());
+            statement.setInt(2,dayStatistic.getDayCount());
+            statement.executeUpdate();
+            return true;
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+        return false;
+    }
+    public static ArrayList<ProductShow> getDayStatistic(int dayCount, String login){
+        ArrayList<ProductShow> productShows = new ArrayList<>();
+        String SQL = "select p.name, pa.amountinstock , pa.amountsold from product_amounts pa\n" +
+                "join products p on pa.product_name = p.name\n" +
+                "join day_statistics ds on pa.day_statistic_id = ds.id\n" +
+                "where ds.day_count = ? and ds.user_login = ?;";
+        try(Connection connection = connect();
+            PreparedStatement statement = connection.prepareStatement(SQL);) {
+            statement.setInt(1,dayCount);
+            statement.setString(2,login);
+            try(ResultSet rs = statement.executeQuery()) {
+                while (rs.next()){
+                    productShows.add(new ProductShow(rs.getString("name"), rs.getInt("amountinstock"), rs.getInt("amountsold")));
+                }
+            }
+            statement.executeUpdate();
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+        return productShows;
+    }
+    public static ArrayList<ProductShow> getPeriodStatisticForProduct(int a, int b, String login, String productName){
+        ArrayList<ProductShow> productShows = new ArrayList<>();
+        String SQL = "select p.name, pa.amountinstock , pa.amountsold from product_amounts pa\n" +
+                "join products p on pa.product_name = p.name\n" +
+                "join day_statistics ds on pa.day_statistic_id = ds.id\n" +
+                "where ds.day_count >= ? and ds.day_count <= ? and ds.user_login = ? and p.name = ?;";
+        try(Connection connection = connect();
+            PreparedStatement statement = connection.prepareStatement(SQL);) {
+            statement.setInt(1,a);
+            statement.setInt(2,b);
+            statement.setString(3,login);
+            statement.setString(4,productName);
+            try(ResultSet rs = statement.executeQuery()) {
+                while (rs.next()){
+                    productShows.add(new ProductShow(rs.getString("name"), rs.getInt("amountinstock"), rs.getInt("amountsold")));
+                }
+            }
+            statement.executeUpdate();
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+        return productShows;
+    }
+    public static int getDayStatisticCount(int dayCount, String login){
+        String SQL = "select * from day_statistic where day_count = ? and login = ?";
+        int ans = 0;
+        try(Connection connection = connect();
+            PreparedStatement statement = connection.prepareStatement(SQL);) {
+            statement.setInt(1, dayCount);
+            statement.setString(1,login);
+            try(ResultSet rs = statement.executeQuery()) {
+                rs.next();
+                ans = rs.getInt("id");
+            }
+            statement.executeUpdate();
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+        return ans;
+    }
+    public static ArrayList<ProductAmount> getThisDayStatistic(int dayCount, String login){
+        int id = getDayStatisticCount(dayCount, login);
+        ArrayList<ProductAmount> productAmounts = new ArrayList<>();
+        String SQL = "select * product_amount where day_statistic_id = ?";
+        try(Connection connection = connect();
+            PreparedStatement statement = connection.prepareStatement(SQL);) {
+            statement.setInt(1,id);
+            try(ResultSet rs = statement.executeQuery()) {
+                while (rs.next()){
+                    productAmounts.add(new ProductAmount(rs.getString("product_name"), rs.getInt("day_statistic_id")
+                    , rs.getInt("amountInStock"), rs.getInt("amountSold")));
+                }
+            }
+            statement.executeUpdate();
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+        return productAmounts;
     }
 }
--- src/main/java/com/outInformation/ProductShow.java
+++ src/main/java/com/outInformation/ProductShow.java
@@ -1,4 +1,4 @@
-package com.model;
+package com.outInformation;
 
 import lombok.*;
 import lombok.experimental.FieldDefaults;
@@ -9,10 +9,8 @@ import lombok.experimental.FieldDefaults;
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @ToString
-public class ProductAmount {
-    //int id;
-    String productName;
-    int dayStatisticId;
+public class ProductShow {
+    String name;
     int amountInStock;
     int amountSold;
 }
